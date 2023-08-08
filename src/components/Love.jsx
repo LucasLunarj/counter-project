3908c20c528635f1813436bf15a71756da35ad68
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-08-08 01:25:38 -0300

    New Changes

--- src/components/Love.jsx
+++ src/components/Love.jsx
@@ -1,8 +1,8 @@
 export const Love = () => {
     return (
-        <div>
+        <div className="flex flex-col">
             <h1>❤️25/06/2021❤️</h1>
-            <a href="https://www.youtube.com/watch?v=ljArIjz_W3Q">🤟Vicky</a>
+            <a className="text-center" href="https://www.youtube.com/watch?v=ljArIjz_W3Q">🤟Vicky</a>
         </div>
     )
 }
